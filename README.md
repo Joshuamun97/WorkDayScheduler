5a1957923debb79906c38856c0eb4ca839d15100
Joshua_Muniz <joshua.mun97@gmail.com>
2022-12-13 18:25:25 -0700

    updated javascript to finalize class changer for time blocks. Updated Read Me

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# WorkDayScheduler
+# WorkDayScheduler#
+
+## Description
+
+Wow! So Much to do, So little Time! How will you ever stay organized during your work day? Fret not! With this work day scheduler, you can interactively schedule your day. This schedule has a built in date/time to help you keep an eye on the time of day and your time blocks. We have also made the time affect the time blocks background color so you can know exactly which tasks have past, which task you should currently be on, and which tasks are coming up in the future! Worried about losing your text inputs? No worries! we have a local storage built in so even if you refresh/close the browser, the next time you open it up everything will still be there! That is, if you press the save button of course. 
+
+## Installation
+
+Below are the installation steps:
+1. Create your variables. 
+2. Decide on what functions are needed and write out your skeleton functions as placeholders to return to
+3. Begin diving into the function arguments.
+4. Make sure to create a function that actually runs the code you are trying to create. Otherwise nothing will execute!
+
+## Usage
+
+This can be used to aide you throughout your work day. Keep track of past, present, and future activities. Below Photo does not show change of color based on time of day. either play with the code or open during busniess hours to see the results!
+
+
+![alt text](/assets/Screenshot%202022-12-13%20at%206.22.08%20PM%20(2).png)
+
+
+## Credits
+
+## License
+
+N/A
+
+---
--- script.js
+++ script.js
@@ -39,7 +39,6 @@ $(function () {
   var today = dayjs();
   $('#date').text(today.format('dddd, MMMM D YYYY, h:mm a'))
   var hour = dayjs().hour();
- //Need to create function to paresint string value of block to numerical value to compare in if statement
 
   $('#hour-9').click(function(){
     $(this)
@@ -66,11 +65,11 @@ $(function () {
   } else {
     $('#hour-10').addClass('future')
   }
+
   $('#hour-11').click(function(){
     $(this)
     localStorage.setItem('#hour-11', elevenAM.value);
     localStorage.getItem('#hour-11');
-   // console.log(document.querySelector('#hour-11').dataset.hour);
   })
   if (hourEleven < hour) {
     $('#hour-11').addClass('past')
@@ -79,6 +78,7 @@ $(function () {
   } else {
     $('#hour-11').addClass('future')
   }
+
   $('#hour-12').click(function(){
     $(this)
     localStorage.setItem('#hour-12', twelvePM.value);
@@ -91,6 +91,7 @@ $(function () {
   } else {
     $('#hour-12').addClass('future')
   }
+
   $('#hour-1').click(function(){
     $(this)
     localStorage.setItem('#hour-1', onePM.value);
@@ -103,6 +104,7 @@ $(function () {
   } else {
     $('#hour-1').addClass('future')
   }
+
   $('#hour-2').click(function(){
     $(this)
     localStorage.setItem('#hour-2', twoPM.value);
@@ -115,6 +117,7 @@ $(function () {
   } else {
     $('#hour-2').addClass('future')
   }
+
   $('#hour-3').click(function(){
     $(this)
     localStorage.setItem('#hour-3', threePM.value);
@@ -127,6 +130,7 @@ $(function () {
   } else {
     $('#hour-3').addClass('future')
   }
+
   $('#hour-4').click(function(){
     $(this)
     localStorage.setItem('#hour-4', fourPM.value);
@@ -152,17 +156,4 @@ $(function () {
     } else {
       $('#hour-5').addClass('future')
     }
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
 });
